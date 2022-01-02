6c63d812efa368ef0224ae694ccc94c58d6b4847
˜Renato˜ <renato.melo@ab-inbev.com>
2022-01-02 20:59:10 -0300

    refactor: update initials page and initials api

--- src/pages/api/initials.ts
+++ src/pages/api/initials.ts
@@ -1,8 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { initialsUseCases } from '@api/pokemons/initials'
+// import { Pokemon } from 'pokenode-ts'
+import { Pokedex } from 'src/common/libs/pokeApi'
 
 export default async function products (request: NextApiRequest, response: NextApiResponse) {
-  const initials = await initialsUseCases.getInitials()
+  const bulbasaur = await Pokedex.getPokemonById(1)
+  const charmander = await Pokedex.getPokemonById(4)
+  const squirtle = await Pokedex.getPokemonById(7)
+
+  const initials = await Promise.all([bulbasaur, charmander, squirtle])
 
   return response.status(200).json(initials)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,63 @@
 // import axios from 'axios'
-import { initialsUseCases } from "@api/pokemons/initials";
-import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import { initialsUseCases } from '@api/pokemons/initials'
+import { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
 // import { PokemonType } from 'src/common/types/types'
 // import {Pokedex} from 'pokeapi-js-wrapper'
-import { getCLS, getFID, getLCP } from "web-vitals";
+import { getCLS, getFID, getLCP } from 'web-vitals'
+// import { useRouter } from 'next/router';
 
 // import { Card, Figure, Wrapper } from '@styles/pages/home'
 
 export default function HomePage() {
-  const [pokemons, setPokemons] = useState<any[]>([]);
-  const { isLoading, error, data } = useQuery("initials", () =>
-    initialsUseCases.getInitials()
-  );
+  const [pokemons, setPokemons] = useState<any[]>([])
+  const { isLoading, error, data } = useQuery('initials', () => initialsUseCases.getInitials())
+  // const router = useRouter()
+
+  // useEffect(() => {
+  //   const token = document.cookie
+
+  //   console.log({ token })
+
+  //   if (!token) router.push('/auth/signin')
+  // }, [])
 
   useEffect(() => {
-    if (!data) return;
+    if (!data) return
 
-    const list = data;
+    const list = data
 
-    setPokemons(list);
-  }, [data]);
+    setPokemons(list)
+  }, [data])
 
   useEffect(() => {
-    console.log({ pokemons });
-    getCLS(console.log);
-    getFID(console.log);
-    getLCP(console.log);
-  }, [pokemons]);
+    console.log({ pokemons })
+    getCLS(console.log)
+    getFID(console.log)
+    getLCP(console.log)
+  }, [pokemons])
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Something went wrong!</p>;
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Something went wrong!</p>
 
   return (
     <section
       style={{
-        color: "white",
-        display: "flex",
-        justifyContent: "space-around",
-        width: "70%",
-        margin: "0 auto",
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'space-around',
+        width: '70%',
+        margin: '0 auto',
       }}
     >
       {pokemons.map((pokemon) => {
         return (
-          <section
-            key={pokemon.id}
-            style={{ textAlign: "center", padding: "20px" }}
-          >
-            <img
-              src={pokemon.image}
-              alt={pokemon.name}
-              style={{ width: "200px" }}
-            />
+          <section key={pokemon.id} style={{ textAlign: 'center', padding: '20px' }}>
+            <img src={pokemon.image} alt={pokemon.name} style={{ width: '200px' }} />
             <p>{pokemon.name}</p>
           </section>
-        );
+        )
       })}
     </section>
-  );
+  )
 }
